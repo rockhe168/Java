4352d2cd7d512c6be9ddb3cab2b5cf9a4853f37f
何湘红 <xhhe@Ctrip.com>
2016-11-25 16:43:02 +0800

    init project

--- quickstart/src/main/java/com/rock/App.java
+++ quickstart/src/main/java/com/rock/App.java
@@ -6,8 +6,28 @@ package com.rock;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static  void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.print("hello world");
+
+        System.out.print("ni hao");
+
+        int temp1=10;
+        int temp2=300;
+        int temp3 = addSum(temp1,temp2);
+
+        String hello ="";
+        if (hello != null) {
+
+        }
+
+        System.out.printf("main end...");
+    }
+
+    public static int addSum(int num1,int num2)
+    {
+        int result = num1+num2;
+
+        return  result;
     }
 }
